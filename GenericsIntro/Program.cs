b4145a94f3795561a5280ad6aa07df2202ef5597
doganbatuhan <batuuhan.dogan@gmail.com>
2021-01-20 23:50:42 +0300

    20 Ocak Ders sonu

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Batu");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Batuhan", "Onur", "Yasin", "Berkay" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            //isimler = new string[5];
+            //isimler[4] = "İhsan";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+            List<string> isimler2 = new List<string> { "Batu", "Onur", "Yasin", "Berkay" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("İhsan");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+            Console.ReadLine();
+        }
+    }
+}
