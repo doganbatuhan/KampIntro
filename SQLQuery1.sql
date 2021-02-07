e635382a89c88c1b4d8c4aec6cd83e52b68db50f
doganbatuhan <batuuhan.dogan@gmail.com>
2021-02-07 17:17:29 +0300

    6.gün ödev 2 için sql sorgusu yazıldı

---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿SELECT p.ProductName as 'Ürün Adı', 
+SUM(p.UnitPrice*od.Quantity) as 'Kazanılan Toplam Miktar' 
+FROM Products p inner join [Order Details] od 
+on p.ProductID = od.ProductID
+inner join Orders o on o.OrderID = od.OrderID
+group by p.ProductName
